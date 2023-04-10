1f168fccfb00650d439f59eb11c4c2e917c53562
wangshangkang <wsk13748116@sina.com>
2023-04-10 17:30:49 +0800

    创建登录页，关联firebase库，使用谷歌登录

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,16 +2,17 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBec-Nn9gg9iVZuhVMIJ5mFhJrpPNbDbWs",
-  authDomain: "one-clothing-db.firebaseapp.com",
-  projectId: "one-clothing-db",
-  storageBucket: "one-clothing-db.appspot.com",
-  messagingSenderId: "447787644630",
-  appId: "1:447787644630:web:4b341414337a83af59bb77"
+  apiKey: "AIzaSyDIB68V_33JIe9yn0r-qo-sG9eMVCrtXvM",
+  authDomain: "sk-clothing-6e728.firebaseapp.com",
+  projectId: "sk-clothing-6e728",
+  storageBucket: "sk-clothing-6e728.appspot.com",
+  messagingSenderId: "907240104281",
+  appId: "1:907240104281:web:982e68f8a607693a2dac48"
 };
 
 // Initialize Firebase
@@ -25,3 +26,29 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// creat db
+export const db = getFirestore();
+
+export const createUserDocumentFormAuth = async(userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapShot = await getDoc(userDocRef);
+
+  // 检查数据是否存在
+  if(!userSnapShot.exists()){
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try{
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createAt
+      });
+    }catch(error){
+      console.log('创建用户报错',error.message);
+    }
+  }
+
+  return userDocRef;
+}
